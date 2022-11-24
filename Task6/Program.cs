169c037956a627eff561d5a6ff166a2450c2682f
kovalevandrey007 <kovalevandrey007@gmail.com>
2022-11-24 20:43:29 +0200

    lost2

---
+++ Task6/Program.cs
@@ -0,0 +1,17 @@
+﻿int GetNumber(string message)
+{
+    bool isCorrect = false;
+    int result = 0;
+
+    while(!isCorrect)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result))
+            isCorrect = true;
+        else
+            Console.WriteLine("Ввели не корректное число. \n1");
+    }
+
+    return result;
+}
